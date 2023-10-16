4433d487e6d5ec89a3ffe00c584b5c35f044810e
gabicilfone <cilfonegabriel@gmail.com>
2023-10-16 09:32:57 -0300

    Add route Home and Us and add Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
       <h1>next</h1>
+
+      <Link href="/us">Us</Link>
     </>
   )
 }
---
+++ pages/us.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+function Us() {
+  return (
+    <div>
+      <Link href="/">Go to Home</Link>
+    </div>
+  )
+}
+
+export default Us
