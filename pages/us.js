c15b92c3441bed7fcd80c69f434210f64bf80768
gabicilfone <cilfonegabriel@gmail.com>
2023-10-17 19:18:10 -0300

    Add page us and create images wit avif and webp

--- pages/us.js
+++ pages/us.js
@@ -1,4 +1,6 @@
+import Image from "next/image"
 import Layout from "../components/layout"
+import styles from "../styles/us.module.css"
 
 function Us() {
   return (
@@ -6,7 +8,27 @@ function Us() {
         title = {"Us"}
         description = "About Us, guitaarLa, music store"
     >
-      <h1>Us</h1>
+      <main className="contenedor">
+        <h2 className="heading">Us</h2>
+        <div className={styles.contenido}>
+          <Image src="/img/nosotros.jpg" width={1000} height={800} alt="Image about us" />
+          <div>
+
+            <p>
+              Aliquam varius ornare quam. Nunc porta eget orci non fringilla. Integer semper ante ut ante consequat rhoncus. 
+              Pellentesque sed odio semper, ullamcorper ipsum at, rhoncus arcu. Proin vel dui odio. Cras consequat vel lorem sed convallis. 
+              Etiam imperdiet imperdiet sapien ac porta. Nulla elit felis, porta et est non, interdum commodo nisl. Maecenas tempor molestie lacinia. 
+  
+            </p>
+
+            <p>
+              Aenean rutrum molestie lacinia. Donec orci ex, tincidunt nec lacinia at, efficitur eu leo. Curabitur interdum, massa sed blandit mattis, nibh arcu lobortis diam, 
+              sed tincidunt nisl eros et augue. Integer et dolor at risus congue vestibulum. Nam efficitur ut ante in semper. Pellentesque finibus porta felis, vitae molestie tellus auctor at. 
+            </p>
+           
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
