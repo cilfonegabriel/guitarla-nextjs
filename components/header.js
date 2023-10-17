03dc582c0c1416f4c6d9f92e20a87a81981c1f56
gabicilfone <cilfonegabriel@gmail.com>
2023-10-17 07:21:13 -0300

    Creating navigation and internal pages

--- components/header.js
+++ components/header.js
@@ -1,11 +1,29 @@
-import Image from "next/future/image"
-import logo from "../public/img/logo.svg"
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Header() {
   return (
     <header>
         <div className='contenedor'>
-            <Image src={logo.src} width={300} height={40} alt="imagen logotipo" />
+            <Image src="/img/logo.svg" width={300} height={40} alt="imagen logotipo" />
+
+            <nav>
+                <Link href="/">
+                    Inicio
+                </Link>
+
+                <Link href="/us">
+                    Us
+                </Link>
+
+                <Link href="/blog">
+                    Blog
+                </Link>
+
+                <Link href="/store">
+                    Store
+                </Link>
+            </nav>
         </div>
     </header>
   )
---
+++ pages/store.js
@@ -0,0 +1,14 @@
+import Layout from "../components/layout"
+
+function Store() {
+  return (
+    <Layout
+        title = {"Virtual Store"}
+        description = "Virtual store, sale of guitars, instruments, GuitarLa"
+    >
+      <h1>Store</h1>
+    </Layout>
+  )
+}
+
+export default Store
