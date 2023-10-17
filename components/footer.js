b591c00b43d06518b7418a6219cf5a986877dc96
gabicilfone <cilfonegabriel@gmail.com>
2023-10-17 18:45:42 -0300

    Add component footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import Link from "next/link"
 
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <footer>
+        <div className='contenedor'>
+            <nav className="navegacion">
+                <Link href="/" >
+                    Inicio
+                </Link>
+
+                <Link href="/us">
+                    Us
+                </Link>
+
+                <Link href="/blog" >
+                    Blog
+                </Link>
+
+                <Link href="/store" >
+                    Store
+                </Link>
+            </nav>
+
+            <p>All rights reserved {new Date().getFullYear()}</p>
+        </div>
+    </footer>
   )
 }
 
