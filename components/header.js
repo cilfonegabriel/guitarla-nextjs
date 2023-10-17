c7897b057e5f27ef31b3b284a6f88fe2e49bacda
gabicilfone <cilfonegabriel@gmail.com>
2023-10-17 07:45:26 -0300

    Add styles to the Header

--- components/header.js
+++ components/header.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from "../styles/header.module.css"
 
 function Header() {
   return (
-    <header>
-        <div className='contenedor'>
+    <header className={styles.header}>
+        <div className={`contenedor ${styles.barra}`}>
             <Image src="/img/logo.svg" width={300} height={40} alt="imagen logotipo" />
 
-            <nav>
+            <nav className={styles.navegacion}>
                 <Link href="/">
+                    
                     Inicio
                 </Link>
 
