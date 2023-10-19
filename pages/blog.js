717bd4f2a6cb9ea644b03ca01f42a61e9fe3b03e
gabicilfone <cilfonegabriel@gmail.com>
2023-10-19 06:56:38 -0300

    Consult the post

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,35 @@
 import Layout from "../components/layout"
+import Post from "../components/post"
 
-function Blog() {
+export default function Blog({posts}) {
+
   return (
     <Layout
         title = {"Blog"}
         description = "Music blog, guitar sales, advice, GuitarLa"
     >
-      <h1>Blog</h1>
+      <main className="contenedor">
+        <h1 className="heading">Blog</h1>
+        <div>
+          {posts?.map(post => (
+            <Post 
+              key={post.id}
+              post={post.attributes}
+            />
+          ))}
+        </div>
+      </main>
     </Layout>
   )
 }
 
-export default Blog
+export async function getServerSideProps() {
+  const resp = await fetch(`${process.env.API_URL}/posts?populate=imagen`)
+  const {data: posts} = await resp.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
