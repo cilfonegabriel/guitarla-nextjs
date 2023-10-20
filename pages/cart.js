0bbaecbb16cb6232b1c82d4ac834d002415d3b26
gabicilfone <cilfonegabriel@gmail.com>
2023-10-20 15:21:57 -0300

    Show content of te cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,11 @@
 import Layout from "../components/layout"
 import styles from "../styles/cart.module.css"
+import Image from "next/image"
 
-function Cart() {
+function Cart({carrito}) {
   return (
     <Layout title="Shopping cart">
-        <main>
+        <main className="contenedor">
             <h1 className="heading">
                 Cart
             </h1>
@@ -12,6 +13,24 @@ function Cart() {
             <div className={styles.contenido}>
                 <div className={styles.carrito}>
                     <h2>Article</h2>
+                
+
+                    {carrito.length === 0 ? 'Empty cart' : (
+                        carrito.map(product => (
+                            <div key={product.id} className={styles.producto}>
+                                <div>
+                                    <Image width={250} height={480} src={product.imagen} alt={product.name}  />
+                                </div>
+
+                                <div>
+                                    <p className={styles.name}>{product.name}</p>
+                                    <p className={styles.price}>$<span>{product.price}</span></p>
+                                    <p className={styles.subtotal}>Subtotal: $<span>{product.amount * product.price}</span></p>
+                                </div>
+                            </div>
+                        ))
+
+                    )}
                 </div>
 
                 <aside className={styles.resumen}>
